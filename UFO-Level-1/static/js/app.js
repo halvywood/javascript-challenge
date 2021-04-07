fe30310cc71fd278001b37c51386799679f0e1a1
halvywood <75034666+halvywood@users.noreply.github.com>
2021-04-06 21:53:52 -0500

    added references to level-1 app.js

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,13 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Viewing the available data fromt he data.js
+console.log(tableData);
+
+
+// Creating References to tbody, input and button
+var $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
