2ea594f96195f4eb9f2005651f5ec3aec20bca93
halvywood <75034666+halvywood@users.noreply.github.com>
2021-04-06 22:50:54 -0500

    update

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,56 +1,65 @@
-// from data.js
+// Step 1: Get the data 
 var tableData = data;
+var tblColumns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
-// Viewing the available data fromt he data.js
-console.log(tableData);
+// Step 2: Create HTML object references
+var tbody = d3.select("tbody");
+var searchDate = d3.select("#searchDate");
+var btnSearch = d3.select("#btnSearch");
+var btnReset = d3.select("#btnReset");
 
-
-// Creating References to tbody, input and button
-var $tbody = d3.select("tbody");
-var button = d3.select("#filter-btn");
-var inputFieldDate = d3.select("#datetime");
-var inputFieldCity = d3.select("#city");
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
-
-
-// Inputing the data into the HTML
-var addData = (dataInput) => {
-    dataInput.forEach(ufoSightings => {
-        var row = $tbody.append("tr");
-        columns.forEach(column => row.append("td").text(ufoSightings[column])
-        )
-    });
+// Step 3: Define an arrow function that builds the HTML table
+// OMG my brain hurts w javascript arrow functions syntax
+var loadTableRows = (whichData) => { // Parameter "whichData" is the data to loop through; makes this re-usable
+	// Step 1: Delete the previously loaded table rows (if there were any)
+    tbody.html("");
+	
+	// Step 2: Loop through "whichData" and add new table row for each row 
+	whichData.forEach(dataRow => { // Loop through each row:
+		// 2.1: Add a new row
+		var tblRow = tbody.append("tr");  
+		
+		// 2.2: Loop through each column and add it as a new table cell
+		tblColumns.forEach(column => tblRow.append("td").text(dataRow[column]))
+	});
 }
 
-addData(tableData);
+// Step 4: call the function (default / first page load only)
+loadTableRows(tableData);
 
-// Creating an Event Listener for the Button
-// Setting up the Filter Button for Date and City
-button.on("click", () => {
+/**********************************
+ Event Listeners 
+**********************************/
 
-    d3.event.preventDefault();
-    
+// Search button - click event
+btnSearch.on("click", () => {
+	// Setup: Prevent the page from refreshing on events
+	d3.event.preventDefault();
+	
+	// 1. Get what the user searched for:
+    var searchedDate = searchDate.property("value");
+	
+	// 2. Filter the data
+    var tableData_Filtered = tableData.filter(tableData => tableData.datetime === searchedDate);
+	
+	// 3. Load the new data
+	if(tableData_Filtered.length !== 0) {
+		loadTableRows(tableData_Filtered);
+	}
+	else {
+		// Clear out the previously loaded HTML:
+		tbody.html("");
+		
+		// Tell them "No rows match"
+		tbody.append("tr").append("td").text("You are unlucky - no sightings on this date");
+	}
+})
 
-    var inputDate = inputFieldDate.property("value").trim();
-    // console.log(inputDate)
-    // trim the inputs 
-    var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
-    
-
-    $tbody.html("");
-
-    let response = {
-        filterDate
-    }
-
-
-    if(response.filterDate.length !== 0) {
-        addData(filterDate);
-    }
-
-    // add comment if not sightings
-    
-        else {
-            $tbody.append("tr").append("td").text("No Sightings Here...Move On...");
-        }
+// Reset button - click 
+btnReset.on("click", () => {
+	// window.location.href = "index.html";
+	document.getElementById("searchDate").value='';
+	
+	// Load original dataset
+	loadTableRows(tableData);
 })
--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -1,51 +1,146 @@
-// from data.js
+// Step 1: Get the data 
 var tableData = data;
+var tblColumns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
-// Creating References to tbody, input and button
-var $tbody = d3.select("tbody");
-var button = d3.select("#filter-btn");
-var inputFieldDate = d3.select("#datetime");
-var inputFieldCity = d3.select("#city");
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+// Step 2: Create HTML object references
+var tbody = d3.select("tbody");
+var btnSearch = d3.select("#btnSearch");
+var btnReset = d3.select("#btnReset");
 
+var searchDate = d3.select("#searchDate");
+var searchCity = d3.select("#searchCity");
+var searchState = d3.select("#searchState");
+var searchCountry = d3.select("#searchCountry");
+var searchShape = d3.select("#searchShape");
 
-// Inputing the data into the HTML
-var addData = (dataInput) => {
-    dataInput.forEach(ufoSightings => {
-        var row = $tbody.append("tr");
-        columns.forEach(column => row.append("td").text(ufoSightings[column])
-        )
-    });
+// Step 3: Define an arrow function that builds the HTML table
+// OMG my brain hurts w javascript arrow functions syntax
+var loadTableRows = (whichData) => { // Parameter "whichData" is the data to loop through; makes this re-usable
+	// Step 1: Delete the previously loaded table rows (if there were any)
+    tbody.html("");
+	
+	// Step 2: Loop through "whichData" and add new table row for each row 
+	whichData.forEach(dataRow => { // Loop through each row:
+		// 2.1: Add a new row
+		var tblRow = tbody.append("tr");  
+		
+		// 2.2: Loop through each column and add it as a new table cell
+		tblColumns.forEach(column => tblRow.append("td").text(dataRow[column]))
+	});
 }
 
-addData(tableData);
+// Step 4: call the function (default / first page load only)
+loadTableRows(tableData);
 
+/**********************************
+ Event Listeners 
+**********************************/
 
-//Create eventListener for button
-button.on("click", () => {
+// Search button - click event
+btnSearch.on("click", () => {
+	// Setup: Prevent the page from refreshing on events
+	d3.event.preventDefault();
+	
+	// Business rule: use the first text box w a value as only search term:
+	
+	// 1. Get what the user searched for:
+    var searchedDate = searchDate.property("value");
+    var searchedCity = searchCity.property("value");
+    var searchedState = searchState.property("value");
+    var searchedCountry = searchCountry.property("value");
+    var searchedShape = searchShape.property("value");
+		
+	if(searchedDate){
+		// 2. Filter the data
+		var tableData_Filtered = tableData.filter(tableData => tableData.datetime === searchedDate);
+	
+		// 3. Load the new data
+		if(tableData_Filtered.length !== 0) {
+			loadTableRows(tableData_Filtered);
+		}
+		else {
+			// Clear out the previously loaded HTML:
+			tbody.html("");
+			
+			// Tell them "No rows match"
+			tbody.append("tr").append("td").text("You are unlucky - no sightings on this date");
+		}
+	}
+	else if(searchedCity) {
+		// 2. Filter the data
+		var tableData_Filtered = tableData.filter(tableData => tableData.city === searchedCity);
+	
+		// 3. Load the new data
+		if(tableData_Filtered.length !== 0) {
+			loadTableRows(tableData_Filtered);
+		}
+		else {
+			// Clear out the previously loaded HTML:
+			tbody.html("");
+			
+			// Tell them "No rows match"
+			tbody.append("tr").append("td").text("You are unlucky - no sightings for this city");
+		}
+	}
+	else if(searchedState) {
+		// 2. Filter the data
+		var tableData_Filtered = tableData.filter(tableData => tableData.state === searchedState);
+	
+		// 3. Load the new data
+		if(tableData_Filtered.length !== 0) {
+			loadTableRows(tableData_Filtered);
+		}
+		else {
+			// Clear out the previously loaded HTML:
+			tbody.html("");
+			
+			// Tell them "No rows match"
+			tbody.append("tr").append("td").text("You are unlucky - no sightings for this state");
+		}
+	}
+	else if(searchedCountry) {
+		// 2. Filter the data
+		var tableData_Filtered = tableData.filter(tableData => tableData.country === searchedCountry);
+	
+		// 3. Load the new data
+		if(tableData_Filtered.length !== 0) {
+			loadTableRows(tableData_Filtered);
+		}
+		else {
+			// Clear out the previously loaded HTML:
+			tbody.html("");
+			
+			// Tell them "No rows match"
+			tbody.append("tr").append("td").text("You are unlucky - no sightings for this country");
+		}
+	}
+	else { // searchedShape
+		// 2. Filter the data
+		var tableData_Filtered = tableData.filter(tableData => tableData.shape === searchedShape);
+	
+		// 3. Load the new data
+		if(tableData_Filtered.length !== 0) {
+			loadTableRows(tableData_Filtered);
+		}
+		else {
+			// Clear out the previously loaded HTML:
+			tbody.html("");
+			
+			// Tell them "No rows match"
+			tbody.append("tr").append("td").text("You are unlucky - no sightings for this shape");
+		}
+	}
+})
 
-    d3.event.preventDefault();
-    var inputDate = inputFieldDate.property("value").trim();
-    // console.log(inputDate)
-    var inputCity = inputFieldCity.property("value").toLowerCase().trim();
-    // console.log(inputCity)
-    var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
-    // console.log(filterDate)
-    var filterCity = tableData.filter(tableData => tableData.city === inputCity);
-    // console.log(filterCity)
-    var filterCombinedData = tableData.filter(tableData => tableData.datetime === inputDate && tableData.city === inputCity);
-    // console.log(filterCombinedData)
-    $tbody.html("");
-      let response = {
-          filterDate, filterCity, filterCombinedData
-      }
-      if(response.filterCombinedData.length !== 0) {
-        addData(filterCombinedData);
-    }
-    else if(response.filterCombinedData.length === 0 && ((response.filterDate.length !== 0 || response.filterCity.length !== 0))) {
-      addData(filterDate) || addData(filterCity);
-  }
-  else {
-    $tbody.append("tr").append("td").text("No Sightings Here...Move On...");
-  }
-  })
+// Reset button - click 
+btnReset.on("click", () => {
+	// window.location.href = "index.html";
+	document.getElementById("searchDate").value='';
+	document.getElementById("searchCity").value='';
+	document.getElementById("searchState").value='';
+	document.getElementById("searchCountry").value='';
+	document.getElementById("searchShape").value='';
+	
+	// Load original dataset
+	loadTableRows(tableData);
+})
